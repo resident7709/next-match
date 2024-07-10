5c488d014625793d93e1273c10fc91a5a059d555
resident7709 <resident7709@gmail.com>
2024-07-10 18:03:45 +0300

    Create a presence indicator // 106

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -4,8 +4,9 @@ import Link from 'next/link';
 import { Card, CardFooter, Image } from '@nextui-org/react';
 
 import { Member } from '@prisma/client';
-import { calculateAge, transformImageUrl } from '@/lib/util';
 import LikeButton from '@/components/LikeButton';
+import PresenceDot from '@/components/PresenceDot';
+import { calculateAge, transformImageUrl } from '@/lib/util';
 
 type Props = { member: Member; likeIds: string[] };
 
@@ -29,6 +30,9 @@ export default function MemberCard({ member, likeIds }: Props) {
       <div onClick={preventLinkAction}>
         <div className='absolute top-3 right-3 z-50'>
           <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+        </div>
+        <div className='absolute top-2 left-3 z-50'>
+          <PresenceDot member={member} />
         </div>
       </div>
       <CardFooter className='flex justify-start bg-black overflow-hidden absolute bottom-0 z-10 bg-dark-gradient'>
